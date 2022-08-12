240d9c143fd507a80795191d12ae1548879372d9
Md A M Adnan <70076187+amadnan@users.noreply.github.com>
2022-08-13 03:47:39 +0600

    1036

---
+++ 1036.py
@@ -0,0 +1,15 @@
+from math import sqrt
+x = input().split()
+a = float(x[0])
+b = float(x[1])
+c = float(x[2])
+D = b*b - 4*a*c
+if(D<0):
+    print("Impossivel calcular")
+else:
+    y1 = ((-b + sqrt(D))/(2*a))
+    y2 = ((-b-sqrt(D))/(2*a))
+    print("R1 = %.5f"%y1)
+    print("R2 = %.5f"%y2)
+    
+    
