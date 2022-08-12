364fe06621f929f87d36b32514ba334ff00b5ade
Md A M Adnan <70076187+amadnan@users.noreply.github.com>
2022-08-12 16:57:26 +0600

    1035

--- 1035.py
+++ 1035.py
@@ -1,1 +1,9 @@
-1035.py
+a = input().split()
+A = int(a[0])
+B = int(a[1])
+C = int(a[2])
+D = int(a[3])
+if( B>C and D>A and C+D > A+B and C>0 and D>0 and ((A%2)==0)):
+    print("Valores aceitos")
+else:
+    print("Valores nao aceitos") 
