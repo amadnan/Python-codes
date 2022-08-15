8738977addd1fc47c51d44cab2e74d50d3b69cb6
Md A M Adnan <70076187+amadnan@users.noreply.github.com>
2022-08-16 03:31:55 +0600

    1042

---
+++ 1042.py
@@ -0,0 +1,7 @@
+a = [int(i) for i in input().split(" ")]
+b = sorted(a)
+for i in b:
+    print(i)
+print('')
+for i in a:
+    print(i)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+a = list()
+a = input().split()
+b = list()
+b = sorted(a)
+
+for i in b:
+    print(i)
+
+print('\n')
+
+for i in a:
+    print(i) 
